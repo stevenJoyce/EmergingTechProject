4402584db39f10e7314be608d7edbdbc929df764
Steven <g00362012@gmit.ie>
2020-11-22 23:01:32 +0000

    Created files for project

---
+++ webService.py
@@ -0,0 +1,18 @@
+#https://www.tornadoweb.org/en/stable/
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Demo for Project 2020")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+
